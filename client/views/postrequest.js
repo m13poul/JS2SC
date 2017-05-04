e5069f341b13218c2a420af5769656cbab6448fb
Christos Poulias <christosp1990@gmail.com>
2017-05-04 10:43:09 +0300

    Delete postrequest.js

--- client/views/postrequest.js
+++
@@ -1,8 +0,0 @@
-function Myfunction() {
-    var Slider1 = document.getElementById("VolumeGranular").value;
-    Slider1.addEventListener("oninput", function(data) {
-        $.post("/Users/chrisfunk/documents/new/installationserver2/server.js", {Slider1,  })
-    })
-}
-
-
